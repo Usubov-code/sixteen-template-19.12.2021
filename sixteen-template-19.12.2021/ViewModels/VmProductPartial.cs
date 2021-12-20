7a55850f0dd75bf9a6c4f43c2160760172bd9379
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-20 18:19:15 +0400

    Partial Product

---
+++ sixteen-template-19.12.2021/ViewModels/VmProductPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sixteen_template_19._12._2021.ViewModels
+{
+    public class VmProductPartial
+    {
+        public string Img { get; set; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+    }
+}
